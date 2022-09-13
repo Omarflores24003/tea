5d9ff7faca42852c250b8a2d9e4fe823f0837ff6
Omarflores24003 <112184277+Omarflores24003@users.noreply.github.com>
2022-09-12 20:37:06 -0600

    Add files via upload

---
+++ lab02/lab02-celsius2farenheit.py
@@ -0,0 +1,4 @@
+temperatureCelsius = float(input("cual es la temperatura en celsius?"))
+print(temperatureCelsius)
+temperatureFarenheit = (9/5) * temperatureCelsius + 32 
+print(temperatureFarenheit)
